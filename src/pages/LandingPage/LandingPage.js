dcc1a6b4228c354c3f276e2b5a4aaa567433c144
Oskar Emilsson <oskaremilsson@gmail.com>
2020-11-20 19:27:27 +0100

    Adding img

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Box, Button } from "@material-ui/core";
 
+import headerImage from "assets/header_img.jpg";
+
 export default function LandingPage() {
   return (
     <Box
@@ -9,15 +11,22 @@ export default function LandingPage() {
       alignItems="center"
       justifyContent="center"
       minHeight="100vh"
-      flexDirection="column"
     >
-      <Button
-        variant="contained"
-        color="primary"
-        href={`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}&scope=${process.env.REACT_APP_SPOTIFY_SCOPE}`}
+      <Box
+        maxWidth="600px"
+        display="flex"
+        justifyContent="center"
+        flexDirection="column"
       >
-        Login
-      </Button>
+        <img src={headerImage} alt="header_img" width="100%" />
+        <Button
+          variant="contained"
+          color="primary"
+          href={`https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=code&redirect_uri=${process.env.REACT_APP_SPOTIFY_REDIRECT_URI}&scope=${process.env.REACT_APP_SPOTIFY_SCOPE}`}
+        >
+          Login
+        </Button>
+      </Box>
     </Box>
   );
 }
